952f3f7882834f8a2e9a14b498c05d13e04c8f4f
tozsola <tozsola@gmail.com>
2022-04-09 22:48:05 +0200

    Regisztráció

--- StudentsRegister2/src/main/java/hu/unideb/inf/FXMLStudentsSceneController.java
+++ StudentsRegister2/src/main/java/hu/unideb/inf/FXMLStudentsSceneController.java
@@ -1,15 +1,21 @@
 package hu.unideb.inf;
 
+import hu.unideb.inf.model.Food;
 import hu.unideb.inf.model.Model;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Label;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView ;
+import javafx.scene.input.MouseEvent;
 
-public class FXMLStudentsSceneController {
+import java.net.URL;
+import java.util.*;
+
+public class FXMLStudentsSceneController implements Initializable{
     private Model model;
+    private Food foods;
 
     public void setModel(Model model) {
         this.model = model;
@@ -25,8 +31,36 @@ public class FXMLStudentsSceneController {
     @FXML
     private Tab etterem;
 
+    @FXML
+    private Tab registration;
 
     @FXML
+    private Tab Login;
+
+    @FXML
+    private ChoiceBox<String> myChoiceBox;
+
+    private String[] kajakoma;
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        foods = new Food();
+        ArrayList<String[]> foodd = foods.getList();
+        String list = foodd.toString();
+
+        for (Object obj : foodd) {
+            if (obj instanceof String[]) {
+                String[] strArray = (String[]) obj;
+                myChoiceBox.getItems().add(Arrays.toString(strArray));
+                // System.out.println(obj);
+            }
+
+
+            // myChoiceBox.setOnAction(this::getFood);
+        }
+    }
+
+        @FXML
     void handleButtonPushed(){
         tp.getSelectionModel() .select(kosar);
     }
@@ -37,7 +71,43 @@ public class FXMLStudentsSceneController {
     public void handleLoadButtonPushed(ActionEvent actionEvent) {
     }
 
+    private void showAlertWithDefaultHeaderText() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Regisztráció");
+        alert.setHeaderText("Nemsokára ehetsz, te dagadt");
+        alert.setContentText("A regisztrálás sikeres volt");
+
+        ImageView icon = new ImageView("file:C:/Users/tozso/Desktop/StudentsRegister2/alertFood.jpg");
+
+        icon.setFitHeight(200);
+        icon.setFitWidth(200);
+        alert.getDialogPane().setGraphic(icon);
+        alert.showAndWait();
+    }
+
+
     public void LognGEnyo(ActionEvent actionEvent) {
-        tp.getSelectionModel() .select(etterem);
+        tp.getSelectionModel().select(etterem);
+    }
+
+    public void RegistforFood(ActionEvent actionEvent) {
+        tp.getSelectionModel().select(registration);
+
+    }
+
+    public void GobackForLogin(ActionEvent actionEvent) {
+        showAlertWithDefaultHeaderText();
+        tp.getSelectionModel().select(Login);
+
+    }
+
+    public void ChoiceFood() {
+       //ForKaja.getItems().addAll("Hamburger", "pizza", "gyros");
+
+    }
+
+
+    public void myChoiceBox(MouseEvent mouseEvent) {
     }
 }
+
